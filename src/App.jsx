b4c33b6d69e89486f7850f14c4a687904613665e
Veronica Madorini <veronicamadorini@Veronicas-MacBook-Air.local>
2026-08-26 16:01:00 +0200

    Configure instant 0ms compact tooltip badge for vinyl player

--- src/App.jsx
+++ src/App.jsx
@@ -675,7 +675,7 @@ function VinylPlayer({ trackUri = "spotify:track:7KKWdbcxg0qGtzVSFg8jjX", isPlay
         <div>
             <div 
                 className={`vinyl-player-widget ${isPlaying ? "playing" : ""}`}
-                title={isPlaying ? "Pause The Mountain Piano Documentary" : "Play The Mountain Piano Documentary"}
+                data-tooltip={isPlaying ? "Pause The Mountain Piano Documentary" : "Play The Mountain Piano Documentary"}
             >
             <svg viewBox="0 0 250 200" className="vinyl-svg">
                 <defs>
